6fb25481eec50adf657473c70228e721eae515ca
Dennis Li <denniszxxc@gmail.com>
2018-12-20 21:04:43 +0800

    Initial Commit

---
+++ deliveries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deliveries
+//
+//  Created by Dennis Li on 20/12/2018.
+//  Copyright © 2018年 Dennis Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
